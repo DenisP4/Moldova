f300af045c1e70ee5f2e6fa67308fdb344ca1923
dennn <dennn@DESKTOP-2N6ADQC>
2020-05-21 18:55:42 -0400

    Good job

--- src/chisinau/cahul.java
+++ src/chisinau/cahul.java
@@ -1,5 +1,8 @@
 package chisinau;
 
+import java.util.HashMap;
+import java.util.Map.Entry;
+
 public class cahul {
 	public static void main(String[] args) {
 		
@@ -13,6 +16,30 @@ public class cahul {
 		System.out.println(" Viorica ..");
 
 		System.out.println("Buna Seara ! ");
+		
+
+		// Please review this  and add the iterator
+		HashMap<String, Boolean> map = new HashMap<>();
+
+		map.put("Roses", true);
+		map.put("Tulips", true);
+		map.put("Peonies", true);
+		map.put("Iris", false);
+		map.put(null, null);
+		map.put(null, false);
+
+		System.out.println(map.entrySet()); // all keys and values
+		System.out.println(map.values());
+		System.out.println(map.keySet());// keys
+		System.out.println(map.size()); // 4
+		System.out.println();
+
+		for (Entry<String, Boolean> bouquet : map.entrySet()) {
+			System.out.println(bouquet.getKey() + ": " + bouquet.getValue());
+			
+			System.out.println("Good job Viorica");
+
+		}
 	}
 
 }
